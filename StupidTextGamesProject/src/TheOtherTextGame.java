e5fbf1a966abda0ba6132c36ad99f5f0ce2a2b43
noahfackler18 <noahfackler18@OlinLab204-09.augustana.edu>
2019-09-26 12:19:08 -0500

    Added TheOtherTextGame

---
+++ StupidTextGamesProject/src/TheOtherTextGame.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TheOtherTextGame implements StupidTextGame {
+
+	public String getName() {
+		return "The Other Text Game";
+	}
+	
+	public void play(Scanner console) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("You must choose, left or right?:  ");
+		String hand = scanner.nextLine();
+		if (hand.contains("left")) {
+			System.out.println("YOU FOUND THE JELLY BEAN!");
+		}
+		else {
+			System.out.println("BZZT YOU LOSE");
+		}
+		System.out.println("Thank you for playing.");
+	}
+		
+		
+}
+
+
