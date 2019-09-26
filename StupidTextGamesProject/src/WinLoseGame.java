480a45f8631fb70ed2ffd062be52f8840e468e7a
mkragenbrink16 <mkragenbrink16@OlinLab204-16.augustana.edu>
2019-09-26 11:37:31 -0500

    Added the WinLose game

---
+++ StupidTextGamesProject/src/WinLoseGame.java
@@ -0,0 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class WinLoseGame implements StupidTextGame {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "WinLoseGame";
+	}
+
+	@Override
+	public void play(Scanner console) {
+		// TODO Auto-generated method stub
+		Random randGen = new Random();	
+		int winLose = randGen.nextInt(2);
+		if (winLose == 1) {
+			System.out.println("You Win!!");
+		} else {
+			System.out.println("You Lose!!");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
