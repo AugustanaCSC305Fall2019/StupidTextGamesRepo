ef66305842cba39305d8ae663abc89b459a7b0aa
gabrielvallejo19 <gabrielvallejo19@OlinLab204-07.augustana.edu>
2021-10-19 11:14:32 -0500

    CowTipper add/edited

--- StupidTextGamesProject/src/CowTipper.java
+++ StupidTextGamesProject/src/CowTipper.java
@@ -10,9 +10,8 @@ public class CowTipper implements StupidTextGame {
 	}
 	
 	public void play(Scanner console) {
-		System.out.println("You tresspass into a field of sleeping cows. You try to push one over but are too weak.");
+		System.out.println("You sneak into a field of sleeping cows. You try to push one over but are too weak for any success.");
 		System.out.println("You go home sad. You lose.");		
-		//Hey
 		
 	}
 
